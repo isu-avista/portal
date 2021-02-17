539b01b9ca9f9f81a1be50d1f9841078834f75f8
Isaac Griffith <grifisaa@isu.edu>
2021-02-16 23:45:38 -0700

    updated documentation and ready to release v0.1.3

--- server/setup.py
+++ server/setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name="avista-portal",
-    version="0.1.2",
+    version="0.1.3",
     author="Isaac Griffith",
     author_email="grifisaa@isu.edu",
     description="",
--- server/sphinx/source/conf.py
+++ server/sphinx/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2020-2021, Isaac Griffith, Andrew Christiansen'
 author = 'Isaac Griffith, Andrew Christiansen'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.2'
+release = '0.1.3'
 
 
 # -- General configuration ---------------------------------------------------
